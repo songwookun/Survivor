798b7c0f68b62056bd0c6cdd5db9ca18c75bfcdd
songwookun <144450071+songwookun@users.noreply.github.com>
2025-08-24 22:23:36 +0900

    몬스터 스폰 위치 지정과 오브젝트 풀링

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,11 +3,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+
+    public float gameTime;
+    public float maxGmaeTime = 2 * 10f;
+
+    public PoolManager pool;
     public Player player;
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if(gameTime > maxGmaeTime)
+            gameTime = maxGmaeTime;
+
     }
 
 }
---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using URandom = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnPoint;
+    public SpawnData[] spanData;
+
+    int level;
+    float timer;
+
+    private void Awake()
+    {
+        spawnPoint = GetComponentsInChildren<Transform>();
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.Instance.gameTime / 10f), spanData.Length - 1);
+
+        if (timer > spanData[level].spawnTime)
+        {
+            timer = 0;
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        GameObject enemy = GameManager.Instance.pool.Get(0);
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spanData[level]);
+    }
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public float spawnTime;
+    public int spriteType;
+    public int health;
+    public float speed;
+}
+
