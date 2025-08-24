d9b361bd14267ae372091d4ed1772134a5697839
songwookun <144450071+songwookun@users.noreply.github.com>
2025-08-24 20:36:08 +0900

    Enemy 추가

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,10 +6,14 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -22,5 +26,15 @@ public class Player : MonoBehaviour
     {
         Vector2 nextVec = inputVec.normalized * speed *Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if(inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
