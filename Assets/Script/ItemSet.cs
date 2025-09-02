cccc380d36bc925a4ea93406c7d42e9e7dfc9ba0
songwookun <144450071+songwookun@users.noreply.github.com>
2025-09-02 23:20:47 +0900

    전설 닌자 및 특급 닌자 추가

--- Assets/Script/ItemSet.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemSet", menuName = "SO/LevelUp Item Set")]
-public class ItemSet : ScriptableObject
-{
-    public itemData[] options = new itemData[5];
-}
